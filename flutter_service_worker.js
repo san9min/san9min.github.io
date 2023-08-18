cb328214eb1202779890fe02f9dc72dbc9eaccb2
Sangmin Lee <sangmin23@yonsei.ac.kr>
2023-08-18 17:36:56 +0900

    logo dim

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -16,7 +16,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "e81a0e07b4fcf1049f44b7bc45a7012a
 "assets/assets/icons/kakao_ch.png": "bcfae927026814edf660ce60e208e99c",
 "assets/assets/icons/wine.png": "df51666177026941510de9b9d3165038",
 "assets/assets/images/audrey_logo.png": "b4e1769e3fb8843d73c2a7cf40d0f30a",
-"assets/assets/images/google.png": "489031ad304a43fa25466238b58c5fd7",
+"assets/assets/images/google.png": "39216d73519bca962bd4a01f3e8f4a4b",
 "assets/assets/images/logo.png": "88eb0d73f9df26e09e8a3dab582176a2",
 "assets/assets/images/weblogo.png": "e37e9e6a9e62c12cba841f862c0dc4aa",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
@@ -37,9 +37,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "e81a0e07b4fcf1049f44b7bc45a7012a
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "8fc89c6703f8f400d244e884334a5e3f",
-"/": "8fc89c6703f8f400d244e884334a5e3f",
-"main.dart.js": "2f01a9ee69ef1cf136c7d285026a7b4a",
+"index.html": "384d6de7c77f8b714c39e6d6bb0ba326",
+"/": "384d6de7c77f8b714c39e6d6bb0ba326",
+"main.dart.js": "9cfe014d45e212a4a2db71c60223cc96",
 "manifest.json": "05ab990f2f6a48706af73af5bea2cdc7",
 "version.json": "2b13c32e0e017b4de0e3d27c170c8224"};
 // The application shell files that are downloaded before a service worker can
