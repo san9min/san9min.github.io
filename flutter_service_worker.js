04345b2a3e2249697cf450c9c6a96e71ddaaf869
Sangmin Lee <sangmin23@yonsei.ac.kr>
2023-08-18 16:57:52 +0900

    txt fd pd

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -37,9 +37,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "e81a0e07b4fcf1049f44b7bc45a7012a
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "d703529071db7d29b995d93921af7179",
-"/": "d703529071db7d29b995d93921af7179",
-"main.dart.js": "4ffd76be4be7b0c7bcf46f40cf6f10ff",
+"index.html": "8fc89c6703f8f400d244e884334a5e3f",
+"/": "8fc89c6703f8f400d244e884334a5e3f",
+"main.dart.js": "2f01a9ee69ef1cf136c7d285026a7b4a",
 "manifest.json": "05ab990f2f6a48706af73af5bea2cdc7",
 "version.json": "2b13c32e0e017b4de0e3d27c170c8224"};
 // The application shell files that are downloaded before a service worker can
