1cd8fed41f07c418da9861f944d4ffc37e32065c
Sangmin Lee <sangmin23@yonsei.ac.kr>
2023-08-21 01:11:21 +0900

    permission

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -37,9 +37,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "e81a0e07b4fcf1049f44b7bc45a7012a
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "0e95b17ebdbff96dbeb99e533e26ff53",
-"/": "0e95b17ebdbff96dbeb99e533e26ff53",
-"main.dart.js": "97497cb827fc662fc8ee1d6b15c86ca1",
+"index.html": "7085834087930c24a093e348ea4674d5",
+"/": "7085834087930c24a093e348ea4674d5",
+"main.dart.js": "1507429a607cd90b98d8749d7387acd8",
 "manifest.json": "05ab990f2f6a48706af73af5bea2cdc7",
 "version.json": "2b13c32e0e017b4de0e3d27c170c8224"};
 // The application shell files that are downloaded before a service worker can
