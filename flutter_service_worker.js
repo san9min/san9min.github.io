7f628820740fad013e4c2eed950f419639b1a046
Sangmin Lee <sangmin23@yonsei.ac.kr>
2023-08-21 01:25:11 +0900

    sa

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -37,8 +37,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "e81a0e07b4fcf1049f44b7bc45a7012a
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "7085834087930c24a093e348ea4674d5",
-"/": "7085834087930c24a093e348ea4674d5",
+"index.html": "9f202528dd025aff7cbdd0484e8b4200",
+"/": "9f202528dd025aff7cbdd0484e8b4200",
 "main.dart.js": "1507429a607cd90b98d8749d7387acd8",
 "manifest.json": "05ab990f2f6a48706af73af5bea2cdc7",
 "version.json": "2b13c32e0e017b4de0e3d27c170c8224"};
